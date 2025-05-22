98ff9203c91bb126b51b32627b7cf2eb755c941d
AvitalHamburg <choenmatilda@gmail.com>
2025-05-22 11:51:07 +0300

    עדכון עמוד 2

--- src/componentsJS/VideoPage .jsx
+++ src/componentsJS/VideoPage .jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../componentsCSS/VideoPage.css';
 import { useLocation } from 'react-router-dom';
-
+import secondPart from '../data/videoData'
 const VideoPage  = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -10,11 +10,11 @@ const VideoPage  = () => {
 
   return (
     <div id="videoPage">
-      <img className="white-cactus" src={`${process.env.PUBLIC_URL}/assets/imgs/white-cactus.png`} />
+      <img className="cactus-img" src={secondPart[prompt].imgSrc} />
 
       <div className="circle-div-video">
-      <h2>קיבלת פרומפט:</h2>
-      <p>{prompt}</p>
+        <h1 className='page-title-video'>{prompt}</h1>
+
        </div>
 
       <div className="footer"></div>
---
+++ src/data/videoData.js
@@ -0,0 +1,122 @@
+// videoData.js
+const videoData = {
+    "החברה החרדית": {
+      title: "החברה החרדית",
+      imgSrc:`${process.env.PUBLIC_URL}/assets/imgs/cuctuseJPNG/cactusDos.png`,
+      videoInfo:`צפו בסרטון הבא להסבר על ההבדלים והאתגרים בחברה החרדית, בסיומו, נשאל שאלות להבנה.`,
+      videoSrc: `${process.env.PUBLIC_URL}/assets/media/dosVid.mp4`,
+      questions: [
+        {
+          question: "שאלה1",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה2",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה3",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה4",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+      ],
+    },
+    "החברה הערבית": {
+      title: "החברה הערבית",
+      videoSrc: `${process.env.PUBLIC_URL}/assets/media/arabVid.mp4`,
+      questions: [
+        {
+          question: "שאלה1",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה2",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה3",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה4",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+      ],
+    },
+    "מוגבלויות והגיל השלישי": {
+      title: "מוגבלויות והגיל השלישי",
+      videoSrc: `${process.env.PUBLIC_URL}/assets/media/oldbVid.mp4`,
+      questions: [
+        {
+          question: "שאלה1",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה2",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה3",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+        {
+          question: "שאלה4",
+          answer1: "תשובה א",
+          answer2: "תשובה ב",
+          answer3: "תשובה ג",
+          answer4: "תשובה ד",
+          correctAns: "תשובה ב",
+        },
+      ],
+    },
+  };
+  
+  export default videoData;
+  
